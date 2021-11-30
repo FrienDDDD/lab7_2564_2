e16f2c49ba102c4ba89fc26ff044874a9dd7f5a3
friend16953 <friendjunk@gmail.com>
2021-11-30 15:19:26 +0700

    lab7_4.cpp complete

--- lab7_4.cpp
+++ lab7_4.cpp
@@ -9,10 +9,10 @@ int main(){
 	int id[] = {123,196,225,329,420,451,515,666,690,787};
 	double gpa[] = {1.50,2.12,4.00,3.69,2.50,2.76,3.54,2.14,3.50,1.88};
 
-    double GPA ;
+   	double GPA ;
 	cout << "Please input GPA: " ;
 	cin >> GPA ;	
-	int i = 0 ;
+    int i = 0 ;
 	while(i < 10){
 		if(gpa[i] >= GPA) {
 			cout << id[i] << " " ;
@@ -21,6 +21,5 @@ int main(){
 		}
 	i ++ ;
 	}
-
 	return 0;
 }
